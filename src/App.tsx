5724a2ff25b61fb394887c241e35897bc08b12da
Ryan Bliss <smile@ryanbliss.me>
2023-04-27 16:55:49 -0700

    Updated app.tsx for teams-js initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
     teamsLightTheme,
-    // teamsDarkTheme,
-    // teamsHighContrastTheme,
+    teamsDarkTheme,
+    teamsHighContrastTheme,
     FluentProvider,
 } from "@fluentui/react-components";
-import { AppRoutes } from "./constants";
+import { AppRoutes, IN_TEAMS } from "./constants";
 import {
     HomePage,
     TeamsConfigPage,
@@ -15,14 +15,73 @@ import {
     BrowseHomePage,
     BrowseKanbanBoardPage,
 } from "./pages";
+import { useEffect, useRef, useState } from "react";
+import { app } from "@microsoft/teams-js";
 
 function App() {
+    const startedInitializingRef = useRef(false);
+    const [initialized, setInitialized] = useState(false);
+    const [teamsTheme, setTeamsTheme] = useState(teamsLightTheme);
+
+    useEffect(() => {
+        // This hook should only be called once, so we use a ref to track if it has been called.
+        // This is a workaround for the fact that useEffect is called twice on initial render in React V18.
+        // In production, you might consider using React Suspense if you are using React V18.
+        // We are not doing this here because many customers are still using React V17.
+        // We are monitoring the React Suspense situation closely and may revisit in the future.
+        if (startedInitializingRef.current) return;
+        startedInitializingRef.current = true;
+        
+
+        if (!IN_TEAMS) return;
+
+        const initialize = async () => {
+            try {
+                console.log("App.tsx: initializing client SDK");
+                await app.initialize();
+                console.log("App.tsx: initialized client SDK");
+                app.notifyAppLoaded();
+                app.notifySuccess();
+                setInitialized(true);
+                const context = await app.getContext();
+                const curTheme = context.app.theme;
+                switch(curTheme) {
+                    case "dark":
+                        setTeamsTheme(teamsDarkTheme);
+                        break;
+                    case "contrast":
+                        setTeamsTheme(teamsHighContrastTheme);
+                        break;
+                    case "default":
+                    default:
+                        setTeamsTheme(teamsLightTheme);
+                        break;
+                }
+                app.registerOnThemeChangeHandler((theme: string | undefined) => {
+                    if (theme == "dark") {
+                        setTeamsTheme(teamsDarkTheme);
+                    } else if (theme == "contrast") {
+                        setTeamsTheme(teamsHighContrastTheme);
+                    } else {
+                        setTeamsTheme(teamsLightTheme);
+                    }
+                });
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        initialize();
+    });
+
+    const appReady = (IN_TEAMS && initialized) || !IN_TEAMS;
+    if (!appReady) return null;
+
     return (
         <FluentProvider
-            theme={teamsLightTheme}
-            // style={{
-            //     backgroundColor: "transparent",
-            // }}
+            theme={teamsTheme}
+            style={{
+                backgroundColor: IN_TEAMS ? "transparent" : undefined,
+            }}
         >
             <Router window={window} basename="/">
                 <Routes>
